2e0c00dd87e2e9284e495da46ba0d603b98c02c0
@yuri_assuncx <killerkoll2012@gmail.com>
2024-09-05 20:53:51 -0300

    fix: new improvements added to Positioning section

--- sections/Positioning.tsx
+++ sections/Positioning.tsx
@@ -58,7 +58,62 @@ const DEFAULT_PROPS: Props = {
     rightText: "Highly Opinionated",
     topText: "Affordable",
     bottomText: "Premium",
-    competitors: [],
+    competitors: [
+      {
+        "source":
+          "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/pitch-template/8ad9f71d-7218-48ef-b225-c348cd169bfb/Figma.png",
+        "description": "Figma",
+        "width": 64,
+        "height": 64,
+        "x_positioning": 10,
+        "y_positioning": 165,
+      },
+      {
+        "source":
+          "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/pitch-template/cafddbfa-83d9-454b-b764-0396fa3ac59f/Canva.png",
+        "description": "Canva",
+        "width": 64,
+        "height": 64,
+        "x_positioning": 85,
+        "y_positioning": 55,
+      },
+      {
+        "source":
+          "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/pitch-template/89907d5f-1ddf-494d-8ffa-ae2d572b5333/Icon.png",
+        "description": "Icon",
+        "width": 64,
+        "height": 64,
+        "y_positioning": -130,
+        "x_positioning": 80,
+      },
+      {
+        "source":
+          "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/pitch-template/3bfa4b08-60cf-4673-9eaf-7cea26a1acb1/Google.png",
+        "description": "Google",
+        "width": 47,
+        "height": 64,
+        "y_positioning": -270,
+        "x_positioning": 200,
+      },
+      {
+        "source":
+          "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/pitch-template/16c5f2d8-939f-4bfd-b8db-8189ad1849ce/Second-Icon.png",
+        "description": "Second Icon",
+        "width": 64,
+        "height": 51,
+        "x_positioning": 350,
+        "y_positioning": 50,
+      },
+      {
+        "source":
+          "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/pitch-template/1e48de3f-21d9-4cc3-b9b6-4c59663b6544/PITCHTEMPLATE.png",
+        "height": 60,
+        "width": 120,
+        "description": "Pitch Template",
+        "y_positioning": -265,
+        "x_positioning": 400,
+      },
+    ],
   },
 };
 
@@ -66,7 +121,7 @@ export default function Positioning(props: Props) {
   const { title, subtitle, positioning } = { ...DEFAULT_PROPS, ...props };
 
   return (
-    <div class="w-full min-h-[982px] flex items-center justify-center gap-14 px-24 py-20 bg-secondary font-inter text-base-100">
+    <div class="w-full min-h-[982px] flex items-center justify-center gap-14 px-24 py-20 bg-secondary font-inter text-base-100 relative">
       <div class="flex flex-col gap-6 max-w-[480px]">
         <h2 class="text-[64px] leading-[80px] font-semibold">
           {title}
