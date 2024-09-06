c7a09d9e15bc517c9e0a0b1c832238ac398c832a
@yuri_assuncx <killerkoll2012@gmail.com>
2024-09-06 15:41:35 -0300

    style (IdealCustomerProfile): UI/UX improved

--- sections/IdealCustomerProfile.tsx
+++ sections/IdealCustomerProfile.tsx
@@ -26,12 +26,12 @@ export default function IdealCustomerProfile(props: Props) {
 
   return (
     <div class="flex items-center w-full min-h-[982px] px-24 py-20 font-inter">
-      <div class="flex flex-col gap-20 max-w-[760px] w-full leading-[80px] font-semibold text-base-100">
-        <h2 class="text-[64px]">{title}</h2>
+      <div class="flex flex-col gap-20 max-w-[760px] w-full leading-[80px] font-semibold">
+        <h2 class="text-[64px] text-primary">{title}</h2>
 
         <ul class="flex flex-wrap items-center w-full gap-2">
           {tags?.map((tag) => (
-            <li class="flex items-center justify-center text-center h-12 bg-primary p-4 text-2xl leading-[25px] rounded-2xl pointer-events-none">
+            <li class="flex items-center justify-center text-center text-base-100 h-12 bg-primary p-4 text-2xl leading-[25px] rounded-2xl pointer-events-none">
               {tag}
             </li>
           ))}
